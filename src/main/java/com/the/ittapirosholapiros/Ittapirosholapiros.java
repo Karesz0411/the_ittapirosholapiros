70101a61681ea730c826a81c090d5287efe7298c
Karesz0411 <91952234+Karesz0411@users.noreply.github.com>
2023-03-18 12:03:38 +0100

    initialize

---
+++ src/main/java/com/the/ittapirosholapiros/Ittapirosholapiros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.the.ittapirosholapiros;
+
+/**
+ *
+ * @author Hallgató
+ */
+public class Ittapirosholapiros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
