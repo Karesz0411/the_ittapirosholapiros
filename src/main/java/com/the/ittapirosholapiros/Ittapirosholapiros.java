4d03ce16ff95d7f2bdc4b9e1cdd4788d54bae369
Karesz0411 <91952234+Karesz0411@users.noreply.github.com>
2023-03-24 14:19:15 +0100

    Update Ittapirosholapiros.java

--- src/main/java/com/the/ittapirosholapiros/Ittapirosholapiros.java
+++ src/main/java/com/the/ittapirosholapiros/Ittapirosholapiros.java
@@ -1,16 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.the.ittapirosholapiros;
 
-/**
- *
- * @author Hallgató
+/*
+ * @author Mihály Károly
  */
 public class Ittapirosholapiros {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    }
+    
+    public class Game {
+        static void initialize(){
+            
+        }
     }
 }
