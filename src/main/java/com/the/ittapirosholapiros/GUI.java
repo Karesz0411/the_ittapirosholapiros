a4d8e4e16317d9e0f7ab4fa0bca2bb2a83eae610
Karesz0411 <91952234+Karesz0411@users.noreply.github.com>
2023-04-12 14:46:29 +0200

    labels added to the gui

--- src/main/java/com/the/ittapirosholapiros/GUI.java
+++ src/main/java/com/the/ittapirosholapiros/GUI.java
@@ -1,18 +1,16 @@
 package com.the.ittapirosholapiros;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.BoxLayout;
+import javax.swing.*;
 import java.awt.*;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
+    /**
+    * Create the class, and the function
+    */
 public class GUI{
     public void display() {
-        int position_x = 100;
-        int position_y = 200;
-        int width = 350;
-        int height = 200;
+        int position_x = 200;
+        int position_y = 50;
+        int width = 1080;
+        int height = 720;
         
         /**
          * Create the GUI frame
@@ -33,5 +31,23 @@ public class GUI{
         gui_panel.setVisible(true);
         gui_frame.add(gui_panel);
         
+        /**
+         * Let's add some label to the panel
+         */
+        JLabel game_title_label = new JLabel("Pick a card, any card");
+        game_title_label.setLocation(430,0);
+        game_title_label.setSize(150,30);
+        game_title_label.setHorizontalAlignment(0);
+        game_title_label.setForeground(Color.RED);
+        gui_panel.add(game_title_label);
+        /**
+         ***********************************************************************
+         */
+        JLabel want_to_play_game = new JLabel("Do you want to play?");
+        want_to_play_game.setLocation(440,200);
+        want_to_play_game.setSize(150,30);
+        want_to_play_game.setHorizontalAlignment(0);
+        want_to_play_game.setForeground(Color.DARK_GRAY);
+        gui_panel.add(want_to_play_game);
     }
 }
